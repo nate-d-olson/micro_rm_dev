933037a755bf876c3ee60b24e246e1a727ffc786
nate-d-olson <nolson@nist.gov>
2014-12-22 17:19:29 -0500

    adding pathoscope merged data

--- analysis/stats/genomic_purity/gather_pathoscope.R
+++ analysis/stats/genomic_purity/gather_pathoscope.R
@@ -9,5 +9,9 @@ pathoscope_params = "../../bioinf/genome_purity/RM8375_pathoscope_pipeline_param
 sampleDF <- parse_pathoparams(pathoscope_params)
 sampleDF$filename <- str_join(genomic_purity_bioinf_dir,sampleDF$sample,"/",
                                 sampleDF$sample,"-sam-report.tsv",sep = "")
+write.csv(sampleDF, "sampleDF.csv")
+
 pathoDF <- ldply(list(sampleDF$filename)[[1]], parse_sam_report)
+write.csv(pathoDF, "pathoDF.csv")
+
 
