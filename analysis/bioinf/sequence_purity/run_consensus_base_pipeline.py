5b089f59757f77c21f969291582fe0cd01c09dff
nathandavidolson@gmail.com <nolson@nist.gov>
2015-01-16 13:40:11 -0500

    added ref to bam name, pipeline and run params, individual run log directories

--- analysis/bioinf/sequence_purity/run_consensus_base_pipeline.py
+++ analysis/bioinf/sequence_purity/run_consensus_base_pipeline.py
@@ -35,7 +35,7 @@ from consensus_base_pipeline import *
 
 # In[4]:
 
-def define_run_params(run_info,parameters):
+def define_run_params(run_info,pipeline_params):
     # hash for storing run specific information
     from collections import defaultdict
     
@@ -45,10 +45,16 @@ def define_run_params(run_info,parameters):
     run_params['run_id'] = run_info_list[0] 
     run_params['vial'] = run_info_list[2]
     run_params['rep'] = run_info_list[3]       
-    run_params['out_dir'] = parameters['root_dir'] + parameters['analysis_out_dir']
-    run_params['log_dir'] = run_params['out_dir'] + "logs"
-    bam_root = parameters['root_dir'] + parameters['analysis_out_dir'] + "tmp/" + run_params['run_id']
-    run_params['bam'] = parameters['root_dir'] + parameters['bam_dir'] + "/" + run_params['run_id'] + ".bam"
+    run_params['out_dir'] = pipeline_params['root_dir'] + pipeline_params['analysis_out_dir']
+
+    # run specific log directory
+    run_params['log_dir'] = run_params['out_dir'] + "logs" + "/" + run_params['run_id']
+    subprocess.call(['mkdir','-p',run_params['log_dir']])
+
+    bam_root = pipeline_params['root_dir'] + pipeline_params['analysis_out_dir'] + "tmp/" + \
+                            pipeline_params['ref_root'] +"_"+ run_params['run_id']
+    run_params['bam'] = pipeline_params['root_dir'] + pipeline_params['bam_dir'] + "/" + \
+                            pipeline_params['ref_root'] +"_"+ run_params['run_id'] + ".bam"
     run_params['fix_file'] = bam_root + "_fix.bam"
     run_params['header_file'] = bam_root + "_header.bam"
     run_params['sort_file'] = bam_root + "_sort.bam"
@@ -57,74 +63,74 @@ def define_run_params(run_info,parameters):
     run_params['intervals_file'] = bam_root + ".intervals"
     run_params['markdup_file'] = bam_root + "_markdup.bam"
     run_params['metrics_file'] = bam_root + ".metrics"
-    run_params['read_group'] = [(("RGID=%s") % parameters['RM']),
+    run_params['read_group'] = [(("RGID=%s") % pipeline_params['RM']),
                                 (("RGLB=%s") % run_params['vial']+"-"+run_params['rep']),
                                 (("RGPL=%s") % run_params['plat']),
                                 (("RGPU=%s") % run_params['vial']+"-"+run_params['rep']),
                                 (("RGSM=%s") % run_params['run_id']),
-                                (("RGCN=%s") % parameters['center'])]
+                                (("RGCN=%s") % pipeline_params['center'])]
     return run_params
 
 
 # In[5]:
 
-def dedup_realign_single_bam(bam_params, consensus_params):
+def dedup_realign_single_bam(run_params, pipeline_params):
     ''' Processing single bam file'''
-    if bam_params['plat'] == "MiSeq":
-        bam_group_sort(in_bam = bam_params['bam'], 
-                       out_bam = bam_params['group_sort_file'], 
-                       log_dir = bam_params['log_dir'])
+    if run_params['plat'] == "MiSeq":
+        bam_group_sort(in_bam = run_params['bam'], 
+                       out_bam = run_params['group_sort_file'], 
+                       log_dir = run_params['log_dir'])
         
-        bam_fixmate(in_bam = bam_params['group_sort_file'],
-                    out_bam = bam_params['fix_file'],
-                    log_dir = bam_params['log_dir'])
+        bam_fixmate(in_bam = run_params['group_sort_file'],
+                    out_bam = run_params['fix_file'],
+                    log_dir = run_params['log_dir'])
 
-        bam_sort(in_bam = bam_params['fix_file'], 
-                out_sort = bam_params['sort_file'], 
-                out_dir = bam_params['log_dir'])
+        bam_sort(in_bam = run_params['fix_file'], 
+                out_sort = run_params['sort_file'], 
+                out_dir = run_params['log_dir'])
     else:
-        bam_add_header(in_bam=bam_params['bam'], 
-                       out_header=bam_params['header_file'],
-                       log_dir=bam_params['log_dir'],
-                       read_group=bam_params['read_group'])
+        bam_add_header(in_bam=run_params['bam'], 
+                       out_header=run_params['header_file'],
+                       log_dir=run_params['log_dir'],
+                       read_group=run_params['read_group'])
 
-        bam_sort(in_bam = bam_params['header_file'], 
-                out_sort = bam_params['sort_file'], 
-                out_dir = bam_params['log_dir'])
+        bam_sort(in_bam = run_params['header_file'], 
+                out_sort = run_params['sort_file'], 
+                out_dir = run_params['log_dir'])
 
-    bam_index(bam = bam_params['sort_file'],
-              out_dir = bam_params['log_dir'])
+    bam_index(bam = run_params['sort_file'],
+              out_dir = run_params['log_dir'])
     
-    bam_realign(ref = consensus_params['ref'],
-                in_bam = bam_params['sort_file'],
-                out_bam = bam_params['realign_file'], 
-                intervals_file = bam_params['intervals_file'],
-                log_dir = bam_params['log_dir'])
+    bam_realign(ref = pipeline_params['ref'],
+                in_bam = run_params['sort_file'],
+                out_bam = run_params['realign_file'], 
+                intervals_file = run_params['intervals_file'],
+                log_dir = run_params['log_dir'])
     
-    bam_markdup(in_bam = bam_params['realign_file'], 
-                out_bam = bam_params['markdup_file'], 
-                metrics_file = bam_params['metrics_file'],
-                log_dir = bam_params['log_dir'])
+    bam_markdup(in_bam = run_params['realign_file'], 
+                out_bam = run_params['markdup_file'], 
+                metrics_file = run_params['metrics_file'],
+                log_dir = run_params['log_dir'])
     
-    bam_index(bam = bam_params['markdup_file'],
-              out_dir = bam_params['log_dir'])
+    bam_index(bam = run_params['markdup_file'],
+              out_dir = run_params['log_dir'])
 
 
 # In[6]:
 
-def run_consensus_base_pipeline(run_params,consensus_params):
+def run_consensus_base_pipeline(run_params,pipeline_params):
     
     ## creating file with run parameters
     run_log_file = open(run_params['out_dir']+"/" + run_params['run_id'] +"-run_parameters.txt", 'w')
     run_log_file.write("Parameter\tValue\n")
     for i in run_params.keys():
         run_log_file.write("%s\t%s\n" % (i, run_params[i]))
-    for i in consensus_params.keys():
-        run_log_file.write("%s\t%s\n" % (i, consensus_params[i]))
+    for i in pipeline_params.keys():
+        run_log_file.write("%s\t%s\n" % (i, pipeline_params[i]))
     run_log_file.close()
     
     ## processing bam
-    dedup_realign_single_bam(run_params, consensus_params)
+    dedup_realign_single_bam(run_params, pipeline_params)
 
 
 # In[28]:
@@ -155,24 +161,24 @@ def read_dat(filename):
 
 def main(filename):
     #read run parameters from input file and process using pathoscope
-    parameters = read_dat(filename)
+    pipeline_params = read_dat(filename)
     
     # creating temp and log directories
-    subprocess.call(['mkdir','-p',parameters['root_dir']+ parameters['analysis_out_dir']+"/tmp/"])
-    subprocess.call(['mkdir','-p',parameters['root_dir']+ parameters['analysis_out_dir']+"/logs/"])
+    subprocess.call(['mkdir','-p',pipeline_params['root_dir']+ pipeline_params['analysis_out_dir']+"/tmp/"])
+    subprocess.call(['mkdir','-p',pipeline_params['root_dir']+ pipeline_params['analysis_out_dir']+"/logs/"])
     
     
     # list of refined bams
     markdup_files = {'PGM':[], 'MiSeq':[]}
-    for i in parameters['datasets'].split(","):
-        run_params = define_run_params(i,parameters)
+    for i in pipeline_params['datasets'].split(","):
+        run_params = define_run_params(i,pipeline_params)
     
         markdup_files[run_params['plat']] += [run_params['markdup_file']]
             
-        run_consensus_base_pipeline(run_params, parameters)
+        run_consensus_base_pipeline(run_params, pipeline_params)
        
     # pileups by platform
-    genome_pileups(parameters, markdup_files)
+    genome_pileups(pipeline_params, markdup_files)
 
 if __name__ == '__main__':
     main(sys.argv[1])
