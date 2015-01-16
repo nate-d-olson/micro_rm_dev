844153903c88c0767da2fbc2dd3cb79646d85d66
nathandavidolson@gmail.com <nolson@nist.gov>
2015-01-15 20:10:20 -0500

    inital purity analysis

--- analysis/stats/sequence_purity/gather_vcf.R
+++ analysis/stats/sequence_purity/gather_vcf.R
@@ -1,20 +1,20 @@
 ## Loads vcf files into sqlite database
-library(stringr) 
+#library(stringr) 
+#library(tidyr)
+library(data.table)
 library(dplyr) 
-library(tidyr) 
-library(VariantAnnotation)
 
-vcf_to_tbl <- function(ref, vcf_filename){
-  vcf <- readVcf(vcf_filename, geno=ref)
-  #converting to a datatable
+# library(RSQLite)
+# library(VariantAnnotation)
+
+cbtsv_tosql <- function (tsv_file,db_conn, tbl_name) {
+  pgm <- fread(tsv_file,sep = "\t",header = TRUE,stringsAsFactors = FALSE)
+  pgm <- rename(pgm, PLATDP=DP) # added to fix issue with two DP columns
+  copy_to(vcf_db, pgm, name = "full_pgm", temporary = FALSE, indexes = list("CHROM","POS","SAMPLE")) 
 }
 
 
+pgm_tsv <- "analysis/stats/sequence_purity/RM8375-PGM.tsv"
+vcf_db <- src_sqlite("data/RM8375/RM8375.sqlite", create = TRUE)
 
-
-# purity_stats()
-# calculates general purity statistics for a dataframe
-
-# loading vcfs
-ref = "../../data/RM8375/ref/CFSAN008157.HGAP.fasta"
-vcf_filename = "../../bioinf/sequence_purity/consensus_base_miseq/RM8375-MiSeq.vcf"
+cbtsv_tosql(pgm_tsv, vcf_db,"cb_pgm")
